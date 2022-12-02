6ae1f353cf9cd3cdd35d9fc1313c65811f4e881f
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2022-12-02 17:38:51 -0600

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD , DB_HOST , DB_NAME} = process.env;
+const { DB_USER, DB_PASSWORD , DB_HOST , DB_NAME, PGUSER, PGPASSWORD, PGHOST} = process.env;
 
 const sequelize = process.env.NODE_ENV === "production" ?
     new Sequelize({
@@ -27,7 +27,7 @@ const sequelize = process.env.NODE_ENV === "production" ?
       ssl: true
     })
     :
-    new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {logging: false, native: false,})
+    new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/pokemon`, {logging: false, native: false,})
 
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
   logging: false, // set to console.log to see the raw SQL queries
